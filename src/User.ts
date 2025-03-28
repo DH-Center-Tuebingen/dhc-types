1a3f5411bf5ddb26f71fa52677aec3d2744e038e
Severino <severin.opel@hotmail.com>
2025-03-28 15:03:46 +0100

    removed namespace from user

--- src/User.ts
+++ src/User.ts
@@ -1,25 +1,21 @@
 import { Timestamps } from './Common';
 
-declare namespace User {
+export type User = {
+    id: number;
+    avatar: boolean;
+    avatar_url?: string;
+    nickname: string;
+    name: string;
+    email: string;
+    metadata?: UserMetadata | null;
+    permissions?: string[];
+} & Timestamps;
 
-    export type User = {
-        id: number;
-        avatar: boolean;
-        avatar_url?: string;
-        nickname: string;
-        name: string;
-        email: string;
-        metadata?: UserMetadata | null;
-        permissions?: string[];
-    } & Timestamps;
-
-    export type UserMetadata = {
-        phonenumber: string;
-        orcid: string;
-        department: string;
-        institution: string;
-        field: string;
-        role: string;
-    }
-
+export type UserMetadata = {
+    phonenumber: string;
+    orcid: string;
+    department: string;
+    institution: string;
+    field: string;
+    role: string;
 }
